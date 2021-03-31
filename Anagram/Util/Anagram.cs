70fda2001131f0d189c6afb0d224a26eb7560e62
Ahmad Sulha <sulha_ps3@yahoo.com>
2021-03-31 08:46:55 +0800

    Removed unused code.

--- Anagram/Util/Anagram.cs
+++ Anagram/Util/Anagram.cs
@@ -7,18 +7,17 @@ namespace Anagram.Util
 {
     public class Anagram
     {
-        public static string matchingAnagram = "";
 
         public static bool AnagramStrStr(string str1, string str2)
         {
             bool isAnagram = false;
             string filteredStr1 = str1.Replace(" ", "").ToLower();
             string filteredStr2 = str2.Replace(" ", "").ToLower();
-            DoPermutation(filteredStr1.ToCharArray(), filteredStr2, 0, filteredStr1.Length - 1, ref isAnagram, ref matchingAnagram);
+            DoPermutation(filteredStr1.ToCharArray(), filteredStr2, 0, filteredStr1.Length - 1, ref isAnagram);
             return isAnagram;
         }
 
-        private static void DoPermutation(char[] elements, string stringToCompare, int currentLoop, int maxLength, ref bool isAnagram, ref string matchingAnagram)
+        private static void DoPermutation(char[] elements, string stringToCompare, int currentLoop, int maxLength, ref bool isAnagram)
         {
             if (currentLoop == maxLength)
             {
@@ -28,18 +27,19 @@ namespace Anagram.Util
                 {
                     s += element;
                 }
+
                 if (stringToCompare.Contains(s))
                 {
-                    matchingAnagram = s;
                     isAnagram = true;
-                }    
+                } 
+                
                 return;
             }
 
             for (int i = currentLoop; i <= maxLength; i++)
             {
                 SwapElements(ref elements[currentLoop], ref elements[i]);
-                DoPermutation(elements, stringToCompare, currentLoop + 1, maxLength, ref isAnagram, ref matchingAnagram);
+                DoPermutation(elements, stringToCompare, currentLoop + 1, maxLength, ref isAnagram);
                 SwapElements(ref elements[currentLoop], ref elements[i]);
             }
         }
